85a47da0b5215bfb5eb5a69e5edd19ff8bfd880a
Aayush Niraula <aayushniraula1@gmail.com>
2020-02-23 18:43:13 +0545

    Image infoo

--- info.py
+++ info.py
@@ -1,8 +1,14 @@
 import cv2
-
+#image pixel info
 
 img = cv2.imread('icon.png')
 
 cv2.imshow('Output', img)
+print(img.shape)
 
-cv2
+print("Height pixel values:", img.shape[0])
+print("Width pixel value", img.shape[1])
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
